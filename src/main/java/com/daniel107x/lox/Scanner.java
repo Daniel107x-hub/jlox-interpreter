41dc33908eac16d153db9bb03578062620f68a61
daniel107x-hub <DMejiaV310796@outlook.com>
2025-08-18 15:08:40 -0600

    Scanner: Added support for 2 character lexemes

--- src/main/java/com/daniel107x/lox/Scanner.java
+++ src/main/java/com/daniel107x/lox/Scanner.java
@@ -38,6 +38,18 @@ public class Scanner {
             case '+': addToken(PLUS); break;
             case ':': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
+            case '!':
+                addToken(nextCharacterMatches('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(nextCharacterMatches('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(nextCharacterMatches('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(nextCharacterMatches('=') ? GREATER_EQUAL : GREATER);
+                break;
             default:
                 Lox.error(line, "Unexpected character");
                 break;
@@ -55,6 +67,13 @@ public class Scanner {
     private void addToken(TokenType type, Object literal){
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean nextCharacterMatches(char expected){
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+        current++;
+        return true;
     }
 
     private boolean isAtEnd(){

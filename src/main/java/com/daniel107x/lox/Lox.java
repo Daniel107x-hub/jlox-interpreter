fb8bc16a93106f4d1c054e4d69efbc609a44d873
daniel107x-hub <DMejiaV310796@outlook.com>
2025-08-18 14:29:26 -0600

    Added token class and token types

--- src/main/java/com/daniel107x/lox/Lox.java
+++ src/main/java/com/daniel107x/lox/Lox.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
     static boolean hadError;
@@ -24,6 +25,7 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if(hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -34,12 +36,13 @@ public class Lox {
             String line = reader.readLine(); // CTRL + D = EOF
             if(line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
     private static void run(String source){
         Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.scaneTokens();
+        List<Token> tokens = scanner.scanTokens();
         for(Token token : tokens) System.out.println(token);
     }
 
